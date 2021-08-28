c4c01d746f904297022fbf39f2688b8d1460342c
julileon10 <julio.leon.diaz1@gmail.com>
2021-08-28 12:10:08 -0400

    starting app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridComponent } from './components/grid/grid.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
